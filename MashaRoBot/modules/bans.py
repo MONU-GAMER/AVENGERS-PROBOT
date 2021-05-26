6bf75cb4c59c37459c9499704fd897e145c32909
MONU-GAMER <84650793+MONU-GAMER@users.noreply.github.com>
2021-05-26 21:57:19 +0530

    Update bans.py

--- MashaRoBot/modules/bans.py
+++ MashaRoBot/modules/bans.py
@@ -64,9 +64,9 @@ def ban(update: Update, context: CallbackContext) -> str:
 
     if is_user_ban_protected(chat, user_id, member) and user not in DEV_USERS:
         if user_id == OWNER_ID:
-            message.reply_text("Trying to put me against a God level disaster huh?")
+            message.reply_text("Trying to put me against a God level disaster huh? Bhosdi mere bot master ko ban mat kr 😠 BC 😠")
         elif user_id in DEV_USERS:
-            message.reply_text("I can't act against our own.")
+            message.reply_text("I can't act against our own 😠 BC 😠")
         elif user_id in DRAGONS:
             message.reply_text(
                 "Fighting this Dragon here will put civilian lives at risk."
@@ -77,7 +77,7 @@ def ban(update: Update, context: CallbackContext) -> str:
             )
         elif user_id in TIGERS:
             message.reply_text(
-                "Bring an order from Heroes association to fight a Tiger disaster."
+                "Bring an order from Heroes association to fight a Tiger disaster 😠 mere family member ko ban mat kr 😠 BC 😠"
             )
         elif user_id in WOLVES:
             message.reply_text("Wolf abilities make them ban immune!")
