f6cff368087660c41587dfc3aafaa837a9281932
MONU-GAMER <84650793+MONU-GAMER@users.noreply.github.com>
2021-05-26 22:21:36 +0530

    Create Monumishra.py

---
+++ MashaRoBot/modules/Monumishra.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2021 TeamDaisyX
+
+
+# This file is part of Daisy (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__mod_name__ = "𝐌𝐎𝐍𝐔 𝐌𝐈𝐒𝐇𝐑𝐀"
+__help__ = """
+          》** 𝐀𝐕𝐄𝐍𝐆𝐄𝐑𝐒_𝐎𝐍𝐖𝐄𝐑** 《
+                                          
+ 𝐡𝐢𝐢 𝐢 𝐚𝐦 𝐦𝐨𝐧𝐮 𝐦𝐢𝐬𝐡𝐫𝐚.
+ 𝐈 𝐚𝐦 𝐟𝐫𝐨𝐦 𝐠𝐚𝐲𝐚,𝐛𝐢𝐡𝐚𝐫,𝐢𝐧𝐝𝐢𝐚
+ 𝐈 𝐚𝐦 𝐬𝐭𝐮𝐝𝐞𝐧𝐭 𝐨𝐟 11𝐭𝐡.
+ 𝐈 𝐚𝐦 𝐚 𝐟𝐫𝐞𝐞 𝐟𝐢𝐫𝐞 𝐧𝐨𝐨𝐛 𝐩𝐥𝐚𝐲𝐞𝐫
+ 𝐌𝐲 𝐟𝐫𝐞𝐞 𝐟𝐢𝐫𝐞 𝐢𝐝 - 462391067
+ 𝐀𝐛𝐨𝐮𝐭 𝐦𝐞 𝐦𝐨𝐫𝐞 𝐚𝐭 - @about_monumishra 
+ 𝐌𝐲 𝐠𝐫𝐨𝐨𝐩 - @masti_masala_2
+ 𝐦𝐲 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 - @about_monumishra
+ 𝐦𝐲 𝐮𝐬𝐞𝐫 name- @monumishra_07
+ 𝐌𝐲 𝐭𝐠 𝐢𝐝 - 1157931747
+         👇
+✨𝑹𝒆𝒂𝒍 𝒏𝒂𝒎𝒆-  abhinav Kumar ♧︎︎︎
+✨𝑵𝒊𝒄𝒌𝒏𝒂𝒎𝒆- mishra g, monu,  etc 
+✨ 𝑺𝒖𝒓𝒏𝒂𝒎𝒆- mishra
+✨𝑪𝒍𝒂𝒔𝒔-10 pass♧︎︎︎
+✨𝑨𝒈𝒆- 16♧︎︎︎
+✨𝑩𝒊𝒓𝒕𝒉𝒅𝒂𝒚-  06 October ♧︎︎︎
+✨𝑭𝒓𝒐𝒎- GAYA ♧︎︎︎
+✨𝑺𝒕𝒂𝒕𝒖𝒔- hu mst rahta hu😁♧︎︎︎
+✨𝑯𝒐𝒃𝒃𝒚-  free fire khelte hoo noob hoo😁😁♧︎︎︎
+
+---------------------------------------------------
+."""
+
